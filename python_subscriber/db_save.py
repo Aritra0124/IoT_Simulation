1b88c28c6e5747dc553325c556162d0066755087
aritra <aritrachakraborty0124@gmail.com>
2023-08-21 00:20:42 +0530

    created basic work flow

---
+++ python_subscriber/db_save.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+def save_data(data):
+    client = MongoClient(host="mongo_db", port=27017, username="root", password="rootpassword")
+    db = client['test']
+    table = db.test_table
+    save = table.insert_one(data)
+    print(save)
+    client.close()
