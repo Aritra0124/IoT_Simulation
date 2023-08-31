ce14a9bf37d1d1115cf89fa02117f2e0b810a34e
aritra <aritrachakraborty0124@gmail.com>
2023-08-31 10:40:36 +0530

    Added range endpoint

--- python_app/app.py
+++ python_app/app.py
@@ -1,8 +1,14 @@
 import json
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query, Request
 from pymongo import MongoClient
 from bson import json_util
+from datetime import datetime
+from pydantic import BaseModel
+
+class Data_data(BaseModel):
+    start: str
+    end: str
 
 app = FastAPI()
 
@@ -16,13 +22,28 @@ def connect_database():
 def get_last_data(sensor_id: int):
     sensor_id_to_fetch = sensor_id
 
-    # Query to retrieve the last ten readings for the specified sensor
     projection = {"_id": 0}
     query = {"sensor_id": sensor_id_to_fetch}
     documents = connect_database().find(query, projection).sort([("time", -1)]).limit(10)
-    print(dir(documents))
     json_data = [json.dumps(document, default=json_util.default) for document in documents]
     return json_data
+
+@app.get("/fetch_sensor_readings/")
+async def fetch_sensor_readings(data: Data_data):
+    try:
+        print("Start time:", data.start)
+        print("End time:", data.end)
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid datetime format")
+
+    # Query MongoDB for data within the specified range
+    query = {
+        "time": {"$gte": data.start, "$lte": data.end}
+    }
+    readings = connect_database().find(query)
+    json_data = [json.dumps(reading, default=json_util.default) for reading in readings]
+    return json_data
+
 @app.get("/")
 def read_root():
     return {"Hello": "World Test from local"}
