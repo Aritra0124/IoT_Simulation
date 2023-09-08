57c5a83fcf3c1a16cf3333718de578071fd4db0c
Aritra <aritra@solivatech.com>
2023-09-08 20:22:54 +0530

    improving code

--- python_publisher/mqtt_publisher.py
+++ python_publisher/mqtt_publisher.py
@@ -1,25 +1,19 @@
 from datetime import datetime
-import paho.mqtt_client.client as mqtt_client
+import paho.mqtt.client as mqtt_client
 import json, time
 import random
 from environ import environ
 
-class envData:
+
+class EnvData:
     env = environ.Env()
     environ.Env.read_env()
     MOSQUITTO_BROKER_IP = env('MOSQUITTO_BROKER_IP')
-    MOSQUITTO_BROKER_PORT = env('MOSQUITTO_BROKER_PORT')
+    MOSQUITTO_BROKER_PORT = int(env('MOSQUITTO_BROKER_PORT'))
     MOSQUITTO_BROKER_TOPIC = env('MOSQUITTO_BROKER_TOPIC')
 
-def on_publish(client, userdata, mid):
-    print("Sent a message")
 
-client = mqtt_client.Client("mqtt_publisher")
-client.on_publish = on_publish
-client.connect(envData.MOSQUITTO_BROKER_IP, int(envData.MOSQUITTO_BROKER_PORT))
-client.loop_start()
-
-while True:
+def random_data():
     sensor_id = random.randint(1, 10)
     data = random.randint(1000, 9999)
     current_datetime = datetime.now()
@@ -29,11 +23,48 @@ while True:
         "time": current_datetime.isoformat()
     }
     msg = json.dumps(data)
-    info = client.publish(
-        topic='python/test',
-        payload=msg,  # No need to encode as 'utf-8' since it's already a JSON string
-        qos=0,
-    )
-    info.wait_for_publish()
-    print(f"Published: {msg}")
-    time.sleep(3)
+    return msg
+
+
+def on_publish(client, userdata, mid):
+    print("Sent a message")
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT broker")
+    else:
+        print(f"Connection to MQTT broker failed with code {rc}")
+        print("Retrying in 5 seconds...")
+        time.sleep(5)
+        client.reconnect()
+
+
+def run():
+    client = mqtt_client.Client("mqtt_publisher")
+    client.on_connect = on_connect
+    client.on_publish = on_publish
+
+    while True:
+        try:
+            client.connect(EnvData.MOSQUITTO_BROKER_IP, EnvData.MOSQUITTO_BROKER_PORT)
+            client.loop_start()
+
+            payload = random_data()
+            info = client.publish(
+                topic=EnvData.MOSQUITTO_BROKER_TOPIC,
+                payload=payload,
+                qos=0,
+            )
+            info.wait_for_publish()
+            print(f"Published: {payload}")
+
+            client.disconnect()
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+        time.sleep(3)
+
+
+if __name__ == '__main__':
+    run()
